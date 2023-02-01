b291c699f7ebbfb7b78a4edcc5d0d9714e69745b
Manali Joshi <designsbymanali@gmail.com>
2023-01-31 17:28:11 -0800

    added html and css

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-// $(document).ready(function () {
-//   $('.main').height($(window).height());
-// });
+const card = document.querySelectorAll('.card');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+card.forEach(function (c) {
+  c.addEventListener('click', function (e) {
+    const plantCard = e.currentTarget.dataset.id;
+
+    // modalOverlay.classList.add('.open-modal');
+    // console.log(modalOverlay.classList);
+  });
+});
