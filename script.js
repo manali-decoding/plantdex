711f6cecfc3b8f8bca0f37feeb29905aa0cd627c
Manali Joshi <designsbymanali@gmail.com>
2023-02-03 18:26:10 -0800

    updated 6:30pm on 2/3/23

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const plantSpecs = [
   {
-    id: 1,
+    name: 'snake',
     title: 'Sansevieria / Snake Plant',
     light: 'Shade to partial sun',
     water: 'water when soil dries out',
@@ -13,7 +13,7 @@ const plantSpecs = [
     img: 'images/sansevieria-houseplant.jpeg',
   },
   {
-    id: 2,
+    name: 'peace-lily',
     title: 'peace lily',
     light: 'indoors with natural indirect sunlight',
     water: 'water when top soil feels dry',
@@ -27,15 +27,115 @@ const plantSpecs = [
 const card = document.querySelectorAll('.card');
 const modalOverlay = document.querySelector('.modal-overlay');
 const closeBtn = document.querySelector('.close');
+// let light = document.querySelector('.light');
+// let water = document.querySelector('.water');
+let modalRight = document.querySelector('.modal-right');
+let modalLeft = document.querySelector('.modal-left');
+let modalBody = document.querySelector('.modal-body');
 
 card.forEach(function (c) {
   c.addEventListener('click', function (e) {
     const plantCard = e.currentTarget.dataset.id;
     console.log(plantCard);
-    handleCardButtonClick();
+    if (plantCard === 'peace-lily') {
+      modalOverlay.classList.toggle('open-modal');
+      let desc = displayPlantSpecs();
+      console.log('calling from outside of the fn --- ' + desc);
+      // console.log(description);
+      modalBody.innerHTML = desc;
+
+      // water.innerHTML = desc;
+      // specs.innerHTML = desc;
+    }
+
+    // handleCardButtonClick();
     //console.log(modalOverlay.classList);
   });
 });
+
+// window.addEventListener('DOMContentLoaded', function () {
+//   displayPlantSpecs(plantSpecs);
+// });
+
+function displayPlantSpecs() {
+  // let displaySpecs
+  // console.log(plant);
+  return `<article class="modal-left">
+            <div class="specs">
+              <div class="specTitle">
+                  <h1>light</h1>
+              </div>
+              <div class="specIcon">
+                  <img src="icn/sun.png" alt="" class="plantCard_icn">
+              </div>
+              <article class="description ">
+                  <ul>
+                      <li><span class="specDesc">${plantSpecs[1]['light']}</span></li>
+                  </ul>
+              </article>
+          </div>
+          <div class="specs">
+              <div class="specTitle">
+                  <h1>Water</h1>
+              </div>
+              <div class="specIcon">
+                  <img src="icn/water.png" alt="" class="plantCard_icn">
+              </div>
+              <article class="description">
+                  <ul>
+                      <li><span class="specDesc">${plantSpecs[1]['water']}</span></li>
+                  </ul>
+              </article>
+          </div>
+          <div class="specs">
+              <div class="specTitle">
+                  <h1>temp</h1>
+              </div>
+              <div class="specIcon">
+                  <img src="icn/temp.png" alt="" class="plantCard_icn">
+              </div>
+              <div class="description">
+                  <ul>
+                      <li><span class="specDesc">${plantSpecs[1]['temp']}</span></li>
+                  </ul>
+              </div>
+          </div>
+          <div class="specs">
+              <div class="specTitle">
+                  <h1>soil</h1>
+              </div>
+              <div class="specIcon">
+                  <img src="icn/soil.png" alt="" class="plantCard_icn">
+              </div>
+              <div class="description">
+                  <ul>
+                      <li><span class="specDesc">${plantSpecs[1]['soil'][0]}</span></li>
+                      <li><span class="specDesc">${plantSpecs[1]['soil'][1]}</span></li>
+                  </ul>
+              </div>
+          </div>
+          <div class="specs">
+              <div class="specTitle">
+                  <h1>humidity</h1>
+              </div>
+              <div class="specIcon">
+                  <img src="icn/humidity.png" alt="" class="plantCard_icn">
+              </div>
+              <div class="description">
+                  <ul>
+                      <li><span class="specDesc">${plantSpecs[1]['humidity']}</span></li>
+                  </ul>
+              </div>
+            </div>
+          </article>
+          <article class="modal-right">
+           <img src="${plantSpecs[1]['img']}" alt="peace lily plant image" class="modal_img">
+          </article>`;
+}
+
+//
+// <article class="modal-left">
+// </article>
 
 closeBtn.addEventListener('click', function () {
   //console.log(modalOverlay.classList);
@@ -45,99 +145,6 @@ closeBtn.addEventListener('click', function () {
 function handleCardButtonClick() {
   modalOverlay.classList.toggle('open-modal');
 }
-
-// window.addEventListener('DOMContentLoaded', function () {
-//   displayPlantSpecs(plantSpecs);
-// });
-
-// function displayPlantSpecs(specs) {
-//   let displaySpecs = [specs].map(function(plant){
-// console.log(plant);
-
-//     return '<article class="modal-content">
-//                 <div class="modal-header">
-//                     <h1 class="modal-plantName">${plant.title}</h1>
-
-//                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-//                         <i class="fa fa-times-circle-o modal_closeBtn" aria-hidden="true"></i>
-//                     </button>
-//                 </div><!-- End of Modal-header -->
-//                 <div class="modal-body">
-//                     <div class="modal-left">
-//                         <div class="light specs">
-//                             <div class="specTitle">
-//                                 <h1>${light}</h1>
-//                             </div>
-//                             <div class="specIcon">
-//                                 <img src="icn/sun.png" alt="" class="plantCard_icn">
-//                             </div>
-//                             <div class="description">
-//                                 <ul>
-//                                     <li><span>${plant.light}</span></li>
-//                                 </ul>
-//                             </div>
-//                         </div>
-//                         <div class="water specs">
-//                             <div class="specTitle">
-//                                 <h1>${water}</h1>
-//                             </div>
-//                             <div class="specIcon">
-//                                 <img src="icn/water.png" alt="" class="plantCard_icn">
-//                             </div>
-//                             <div class="description">
-//                                 <ul>
-//                                     <li><span>${plant.water}</span></li>
-//                                 </ul>
-//                             </div>
-//                         </div>
-//                         <div class="temp specs">
-//                             <div class="specTitle">
-//                                 <h1>${temp}</h1>
-//                             </div>
-//                             <div class="specIcon">
-//                                 <img src="icn/temp.png" alt="" class="plantCard_icn">
-//                             </div>
-//                             <div class="description">
-//                                 <ul>
-//                                     <li><span>${plant.temp}</span></li>
-//                                 </ul>
-//                             </div>
-//                         </div>
-//                         <div class="soil specs">
-//                             <div class="specTitle">
-//                                 <h1>${soil}</h1>
-//                             </div>
-//                             <div class="specIcon">
-//                                 <img src="icn/soil.png" alt="" class="plantCard_icn">
-//                             </div>
-//                             <div class="description">
-//                                 <ul>
-//                                     <li><span>${plant.soil[0]}</span></li>
-//                                     <li><span>${plant.soil[1]}</span></li>
-//                                 </ul>
-//                             </div>
-//                         </div>
-//                         <div class="humidity specs">
-//                             <div class="specTitle">
-//                                 <h1>${humidity}</h1>
-//                             </div>
-//                             <div class="specIcon">
-//                                 <img src="icn/humidity.png" alt="" class="plantCard_icn">
-//                             </div>
-//                             <div class="description">
-//                                 <ul>
-//                                     <li><span>#{plant.humidity}</span></li>
-//                                 </ul>
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div class="modal-right">
-//                         <img src="${plant.img}" alt="snake plant image" class="modal_img">
-//                     </div>
-//                 </div> <!-- End of Modal-body -->
-//             </article>';
-
-//   });
 
 // displaySpecs = displaySpecs.join('');
 // console.log(displaySpecs);
